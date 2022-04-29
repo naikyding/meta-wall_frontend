c57ac3774ff279f73b4f83e956724acd32a037ae
NaikyDing <74ding@gmail.com>
2022-04-30 07:01:18 +0800

    feat: 初始化專案設置
    - eslint / prettier
    - stylelint

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { fileURLToPath, URL } from 'url'
+import StylelintPlugin from 'vite-plugin-stylelint'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    StylelintPlugin({
+      fix: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
