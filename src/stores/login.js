730453e676982a38fdee2c43e2d4064163e4dd5c
NaikyDing <74ding@gmail.com>
2022-05-01 21:12:38 +0800

    feat (login/register): 路由設置 && store

--- src/stores/login.js
+++ src/stores/login.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 export const useLoginStore = defineStore('Login', {
   state: () => ({
-    data: 123,
+    activeComponentIndex: 0,
   }),
   getters: {},
   actions: {},
