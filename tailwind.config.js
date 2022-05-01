87748760bc30872372c49aefecd81ae026cdbfa6
NaikyDing <74ding@gmail.com>
2022-05-01 21:11:35 +0800

    feat (css): 客製化設置
    - 字型
    - color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      primary: '#03438D',
+      secondary: '#EEC32A',
+      baseBg: '#EFECE7',
+      black: '#000400',
+      white: '#fff',
+      error: '#F57375',
+    },
+    fontFamily: {
+      logo: ['Paytone One'],
+      dec: ['Helvetica Neue'],
+      input: ['Azeret Mono'],
+    },
+    extend: {
+      boxShadow: {
+        base: '-2px 2px 0px #000400',
+        card: '-8px 8px 0px #00040029',
+      },
+    },
   },
   plugins: [],
 }
