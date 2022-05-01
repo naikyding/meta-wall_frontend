dd6b08871275a2346aa3a853e79826c00779ed36
NaikyDing <74ding@gmail.com>
2022-05-01 22:49:42 +0800

    feat(style): bg grid

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,15 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView class="font-dec text-black" />
+  <div class="bg-grid bg-[length:63px_63px] bg-baseBg bg-center">
+    <RouterView class="font-dec text-black" />
+  </div>
 </template>
+
+<style>
+.bg-grid {
+  background-image:
+    linear-gradient(#70707023 1px, transparent 0),
+    linear-gradient(90deg, #70707023 1px, transparent 0);
+}
+</style>
