934da874661e1d72a7f11520d431d8baad7182d2
Naiky <74ding@gmail.com>
2022-06-03 14:36:00 +0800

    feat: autoprefix setting

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,16 @@ module.exports = {
   plugins: {
     'postcss-import': {},
     tailwindcss: {},
-    autoprefixer: {},
+    autoprefixer: {
+      browserslist: {
+        production: ['>0.2%', 'not dead', 'not op_mini all'],
+        development: [
+          'last 1 chrome version',
+          'last 1 firefox version',
+          'last 1 safari version',
+          'last 1 ie version',
+        ],
+      },
+    },
   },
 }
