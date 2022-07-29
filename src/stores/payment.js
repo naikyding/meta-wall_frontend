7b710942e35ea3f65252fb40c9f18742efa124a2
NaikyDing <74ding@gmail.com>
2022-07-29 18:09:55 +0800

    feat: line pay catch 處理

--- src/stores/payment.js
+++ src/stores/payment.js
@@ -1,16 +1,25 @@
 import { defineStore } from 'pinia'
 import { linePayAPI } from '../api'
+import Swal from 'sweetalert2'
 
 export const useLinePayStore = defineStore('Line Pay', {
   state: () => ({}),
   getters: {},
   actions: {
     async goLinePay(payItem) {
-      // console.log('goLinePay')
-      const { returnCode, info } = await linePayAPI(payItem)
-      if (returnCode === '0000') {
-        console.log(info)
-        location.href = info.paymentUrl.web
+      try {
+        const { returnCode, info } = await linePayAPI(payItem)
+        if (returnCode === '0000') {
+          location.href = info.paymentUrl.web
+        }
+      } catch ({ data }) {
+        Swal.fire({
+          position: 'center',
+          icon: 'error',
+          title: data.message,
+          showConfirmButton: false,
+          timer: 1500,
+        })
       }
     },
   },
